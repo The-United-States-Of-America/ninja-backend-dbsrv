0b21f11fc1ac4ecc70c2f666656188e4bd9206ab
Pranav Sathyanarayanan <sathyp@rpi.edu>
2015-10-28 18:41:53 -0400

    Added API Docs for organizations

--- src/routes/organization.js
+++ src/routes/organization.js
@@ -12,6 +12,33 @@ export default class OrganizationRoute {
    */
   constructor() {
 
+    /**
+     * @api {post} /organization/create/ Create New Organization
+     * @apiName CreateOrganization
+     * @apiGroup Organization
+     *
+     * @apiParam {Object} org The org object that you want to create.
+     * @apiParamExample {json} Request-Example:
+     *     {
+     *       "name": 'Primary Health Care',
+     *       "state": 'NY',
+     *       "address": '123 Test Lane',
+     *       "zip": '12345',
+     *       "phone": "1234567890"
+     *     }
+     * @apiSuccessExample {json} Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+     *       "id": 1,
+     *       "name": 'Primary Health Care',
+     *       "state": 'NY',
+     *       "address": '123 Test Lane',
+     *       "zip": '12345',
+     *       "phone": "1234567890"
+     *     }
+     * @apiSuccess {Object} result JSON Object representing the organization object in the database.
+     * @apiError {String} err An error statement regarding what went wrong.
+     */
     rtr.post('/create', (req, res) => {
       Organization.create(req.body, (org, err) => {
         if(err) return res.status(400).send(err);
